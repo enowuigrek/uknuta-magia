21d876019d2cdd87194ced9e839f53add3d7fe9c
Łukasz Nowak <99438005+enowuigrek@users.noreply.github.com>
2025-06-20 16:37:20 +0200

    Configure Stylelint with camelCase convention and property order

---
+++ .stylelintrc.cjs
@@ -0,0 +1,79 @@
+module.exports = {
+  extends: ['stylelint-config-standard-scss'],
+  plugins: ['stylelint-order'],
+  rules: {
+    'order/properties-order': [
+      [
+        // 1. Positioning
+        'position',
+        'top',
+        'right',
+        'bottom',
+        'left',
+        'z-index',
+
+        // 2. Box Model
+        'display',
+        'flex-direction',
+        'flex-wrap',
+        'justify-content',
+        'align-items',
+        'gap',
+        'width',
+        'min-width',
+        'max-width',
+        'height',
+        'min-height',
+        'max-height',
+        'padding',
+        'padding-top',
+        'padding-right',
+        'padding-bottom',
+        'padding-left',
+        'margin',
+        'margin-top',
+        'margin-right',
+        'margin-bottom',
+        'margin-left',
+        'overflow',
+        'overflow-x',
+        'overflow-y',
+        'box-sizing',
+
+        // 3. Typography
+        'font',
+        'font-family',
+        'font-size',
+        'font-weight',
+        'line-height',
+        'letter-spacing',
+        'text-align',
+        'text-decoration',
+        'text-transform',
+        'white-space',
+        'color',
+
+        // 4. Background / Border / Visuals
+        'background',
+        'background-color',
+        'background-image',
+        'border',
+        'border-radius',
+        'box-shadow',
+        'opacity',
+
+        // 5. Effects / Transitions / Animations
+        'transition',
+        'transform',
+        'animation',
+
+        // 6. Misc
+        'cursor',
+        'visibility'
+      ],
+      {
+        unspecified: 'bottomAlphabetical'
+      }
+    ]
+  }
+};
